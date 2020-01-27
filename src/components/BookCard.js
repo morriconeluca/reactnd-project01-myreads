0875e53e4a4661174b101f3360a8c82468d7ce53
Luca Morricone <morricone.luca@outlook.com>
2020-01-27 11:19:28 +0100

    Add props to BookCard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import BookShelfChanger from './BookShelfChanger';
 
-function BookCard(props) {
+function BookCard({title, authors, shelf, thumbnail}) {
   return (
     <li className="book-card fadeIn">
       <figure className="book">
-        <img className="book-cover" src="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api" alt="Cover of To Kill a Mockingbird" />
-        <BookShelfChanger />
+        <img
+          className="book-cover"
+          src={thumbnail}
+          alt={`Cover of ${title}`}
+        />
+        <BookShelfChanger shelf={shelf} />
       </figure>
       <header className="book-details">
-        <p className="book-authors">Harper Lee</p>
-        <h3 className="book-title">To Kill a Mockingbird</h3>
+          {
+            authors.map(author => (
+              <p className="book-authors">{author}</p>
+            ))
+          }
+        <h3 className="book-title">{title}</h3>
       </header>
     </li>
   );
